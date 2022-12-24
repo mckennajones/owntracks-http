b3fdd2473836a5c32fd50c89aa0c1fed33219287
McKenna Jones <hello@mckennajones.com>
2022-12-24 14:23:31 -0700

    date picker actually works now

--- frontend/src/lib/Day.svelte
+++ frontend/src/lib/Day.svelte
@@ -1,64 +1,65 @@
 <script lang="ts">
   	import { createEventDispatcher } from 'svelte';
+    import { DateInput } from 'date-picker-svelte';
 
     const dispatch = createEventDispatcher();
 
     let nowDate = new Date()
 
-    let days = []
-    let years = []
-    const monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let years = [2022, 2021]
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-    for (let x=2022; x <= nowDate.getFullYear(); x+=1){
-        years.push(x)
-    }
     
-    let day;
-    let month;
-    let year;
+    let date: Date;
+    let day: any;
+    let month: any;
+    let year: any;
 
-    function isLeapYear(year: number): boolean {
-        return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+    function yearChange(e) {
+        date = null;
+        day = null;
+        month = "";
+
+        dateChange();
     }
 
     function monthChange(e) {
-        // TODO
-        const feb = isLeapYear(year) ? 29 : 28
-        
-        days = []
-        for (let x = 1; x <= monthLengths[months.indexOf(month)]; x += 1) {
-            days.push(x)
+        date = null;
+        day = null;
+        if (year == null || year == "") {
+            year = nowDate.getFullYear()
         }
-
+        
         dateChange();
-
-        console.log(day, month, year)
     }
 
     function dayChange(e) {
-        dateChange();
-        console.log(day, month, year)
-    }
+        year = date.getFullYear()
+        month = date.getMonth()
+        day = date.getDate()
 
-    function yearChange(e) {
         dateChange();
-        console.log(day, month, year)
     }
 
     function dateChange() {
         let start: Date;
         let end: Date;
         if (year && month && day) {
-            start = new Date(year, months.indexOf(month), day, 0)
-            end = new Date(year, months.indexOf(month), day, 23, 59, 59)
+            console.log("all set")
+            start = new Date(year, month, day, 0)
+            end = new Date(year, month, day, 23, 59, 59)
         } else if (year && month) {
-            start = new Date(year, months.indexOf(month), 0)
-            end = new Date(year, months.indexOf(month), monthLengths[months.indexOf(month)])
+            console.log("year and month set")
+            // get month length by setting date to 0
+            let tempDate = new Date(year, month + 1, 0)
+            start = new Date(year, month, 0)
+            end = new Date(year, month, tempDate.getDate())
         } else if (year) {
+            console.log("year set")
             start = new Date(year, 0, 1)
-            end = new Date(year, 12, 31)
+            end = new Date(year, 11, 31)
         }
+
+        console.log(start, end)
 
         dispatch('dateChange', {
 			start: start.toISOString(),
@@ -76,20 +77,15 @@
 		</option>
 	{/each}
 </select>
+
 <select name="month" bind:value={month} on:change={monthChange}>
     <option value="" disabled selected hidden>Month</option>
     {#each months as month}
-		<option value={month}>
+		<option value={months.indexOf(month)}>
 			{month}
 		</option>
 	{/each}
 </select>
-<select name="day" bind:value={day} on:change={dayChange}>
-    <option value="" disabled selected hidden>Day</option>
-    {#each days as day}
-		<option value={day}>
-			{day}
-		</option>
-	{/each}
-</select>
+
+<DateInput placeholder="Day" bind:value={date} on:select={dayChange} format="dd" />
 
--- frontend/src/lib/Map.svelte
+++ frontend/src/lib/Map.svelte
@@ -15,20 +15,28 @@
         map = new maplibregl.Map({
             container: 'map', // container id
             style: 'https://api.maptiler.com/maps/streets-v2/style.json?key=9gPJtf3luQ9Ud3c7AHJ6', // style URL
-            center: ["-104.9903", "39.7392"], // starting position [lng, lat]
-            zoom: 10 // starting zoom
+            // center: ["-104.9903", "39.7392"], // starting position [lng, lat]
+            // zoom: 10 // starting zoom
         });
+        map.addControl(new maplibregl.NavigationControl({}));
 
         const locs =  await fetch("http://192.168.0.100:8090/locations");
         const data = await locs.json();
 
+        let bounds = new maplibregl.LngLatBounds();
         data.forEach(point => {
             var marker = new maplibregl.Marker()
                         .setLngLat([point.Lon, point.Lat])
                         .addTo(map)
             
             markers.push(marker)
-        });  
+            bounds.extend([point.Lon, point.Lat])
+        });
+            
+        map.fitBounds(bounds, {
+            padding: 100
+        });
+
     };
 
     async function updateLocations(e) {
@@ -40,16 +48,23 @@
         const start = e.detail.start;
         const end = e.detail.end;
         const url = `http://192.168.0.100:8090/locations?start=${start}&end=${end}`
+        console.log(url)
         const locs =  await fetch(url);
         const data = await locs.json();
 
         markers = []
+        let bounds = new maplibregl.LngLatBounds();
         data.forEach(point => {
             var marker = new maplibregl.Marker()
                         .setLngLat([point.Lon, point.Lat])
                         .addTo(map)
             
             markers.push(marker)
+            bounds.extend([point.Lon, point.Lat])
+        });
+
+        map.fitBounds(bounds, {
+            padding: 100
         });
     };
 
