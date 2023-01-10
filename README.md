6d75cc6bcf4baefe714e4c3182dbf4b0eb4c0dfc
McKenna Jones <hello@mckennajones.com>
2023-01-09 17:21:17 -0700

    readme and remove old datepicker code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # owntracks-http
 A simple HTTP client for the OwnTracks app
+
+## TODO
+
+- Show heatmap on "all time" view
+- put time selection in url
+    - On page load url params should determine time selection
+    - https://stackoverflow.com/questions/69606222/how-to-update-the-page-and-query-parameters-in-sveltekit
--- frontend/src/lib/DatePicker.svelte
+++ frontend/src/lib/DatePicker.svelte
@@ -7,39 +7,6 @@
 
     const dispatch = createEventDispatcher();
 
-    let day: any;
-    let month: any;
-    let year: any;
-
-    function dateChange() {
-        console.log(year, month, day)
-        let start: Date;
-        let end: Date;
-        
-        if (year && month >= 0 && day) {
-            console.log("all set")
-            start = new Date(year, month, day, 0)
-            end = new Date(year, month, day, 23, 59, 59)
-        } else if (year && month >= 0) {
-            console.log("year and month set")
-            // get month length by setting date to 0
-            let tempDate = new Date(year, month + 1, 0)
-            start = new Date(year, month, 0)
-            end = new Date(year, month, tempDate.getDate())
-        } else if (year) {
-            console.log("year set")
-            start = new Date(year, 0, 1)
-            end = new Date(year, 11, 31)
-        }
-
-        console.log(start, end)
-
-        dispatch('dateChange', {
-			start: start.toISOString(),
-            end: end.toISOString()
-		});
-    }
-
     function datePickerChange(event: any) {
         console.log(event)
 
@@ -69,6 +36,6 @@
 </script>
 
 <DatePicker datePickerType="range" on:change={datePickerChange}>
-    <DatePickerInput hideLabel placeholder="mm/dd/yyyy" />
-    <DatePickerInput hideLabel placeholder="mm/dd/yyyy" />
+    <DatePickerInput hideLabel placeholder="All" />
+    <DatePickerInput hideLabel placeholder="time" />
 </DatePicker>
